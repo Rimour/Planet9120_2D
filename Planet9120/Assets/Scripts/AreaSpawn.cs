d94dbd9ee70bf850afebc6e1f983bd85cba158e9
Rimour <63150407+Rimour@users.noreply.github.com>
2020-09-12 13:32:52 -0400

    Added Area Spawn Script

---
+++ Planet9120/Assets/Scripts/AreaSpawn.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaSpawn : MonoBehaviour
+{
+    public GameObject theEnemy;
+    int xPos;
+    int zPos;
+    int yPos;
+    int EnemyCount;
+    public int MaxEnemyCount = 10;
+    public int minXPos;
+    public int maxXPos;
+    public int minYPos;
+    public int maxYPos;
+    public int minZPos;
+    public int maxZPos;
+    
+
+
+    void Start()
+    {
+        StartCoroutine(EnemyDrop());
+    }
+
+    IEnumerator EnemyDrop()
+    {
+        while (EnemyCount < MaxEnemyCount)
+        {
+            xPos = Random.Range(minXPos, maxXPos);
+            zPos = Random.Range(minZPos, maxZPos);
+            yPos = Random.Range(minYPos, maxYPos);
+            Instantiate(theEnemy, new Vector3(xPos, yPos, zPos), Quaternion.identity);
+            yield return new WaitForSeconds(0.1f);
+            EnemyCount += 1;
+        }
+    }
+}
