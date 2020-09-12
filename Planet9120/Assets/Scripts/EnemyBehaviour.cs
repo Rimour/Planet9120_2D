71f308a1636a387d17bf2a3113fb0e7f4f4bd60d
Rimour <63150407+Rimour@users.noreply.github.com>
2020-09-12 16:43:16 -0400

    Added enemy movment script

---
+++ Planet9120/Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    Transform Ship;
+    public float Speed;
+
+    public void Start()
+    {
+        Ship = GameObject.FindWithTag("Ship").transform;
+    }
+    public void Movement()
+    {
+        float MoveSpd = Speed * Time.deltaTime;
+        transform.position = Vector2.MoveTowards(transform.position, Ship.position, MoveSpd);
+    }
+    public void Update()
+    {
+        Movement();
+    }
+}
