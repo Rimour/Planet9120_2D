4180996d430cc23549bb08528097eb7f379aaa53
Rimour <63150407+Rimour@users.noreply.github.com>
2020-09-14 08:05:35 -0400

    updated enemy movement
    enemies will now target player if within range and move towards ship if player is not near

--- Planet9120/Assets/Scripts/EnemyBehaviour.cs
+++ Planet9120/Assets/Scripts/EnemyBehaviour.cs
@@ -5,19 +5,42 @@ using UnityEngine;
 public class EnemyBehaviour : MonoBehaviour
 {
     Transform Ship;
+    Transform Player;
+    //Transform []Towers;
     public float Speed;
+    public float PlayerRange;
+    public float attackRange;
 
     public void Start()
     {
         Ship = GameObject.FindWithTag("Ship").transform;
+        Player = GameObject.FindWithTag("Player").transform;
+        //Towers = GameObject.FindGameObjectsWithTag("Tower").transform;
     }
-    public void Movement()
-    {
-        float MoveSpd = Speed * Time.deltaTime;
-        transform.position = Vector2.MoveTowards(transform.position, Ship.position, MoveSpd);
-    }
+   
     public void Update()
     {
-        Movement();
+        float MoveSpd = Speed * Time.deltaTime;//enemy movement speed
+        float ShipDist = Vector2.Distance(Ship.position, transform.position);//distance between enemy & ship
+        float PlayerDist = Vector2.Distance(Player.position, transform.position);//distance between enemy & player
+        if(PlayerDist <= PlayerRange)// if enemy is within range of player
+        {
+            transform.position = Vector2.MoveTowards(transform.position, Player.position, MoveSpd);//enemy moves towards player
+            if(PlayerDist >= attackRange)
+            {
+                //attack player animation
+                //deal damage to player
+            }
+        }
+        else
+        {
+            transform.position = Vector2.MoveTowards(transform.position, Ship.position, MoveSpd);//enemy moves towards ship
+            if(ShipDist <= attackRange)
+            {
+                //attack player animation
+                //deal damage to ship
+            }
+        }
+        
     }
 }
