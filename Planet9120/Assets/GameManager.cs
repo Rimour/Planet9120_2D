bc4cfe2e7e35345df7a3bcf6b0eba1567d584076
Rimour <63150407+Rimour@users.noreply.github.com>
2020-09-18 08:36:11 -0400

    Updated MainMenu, Game background, UI, & added pause menu
    You can now change scenes from main menu with space. Pressing space in game menu will pause the game. UI has been moved to the botton of screen. Added new fonts. Ship resource count now tracks total amount

--- Planet9120/Assets/GameManager.cs
+++ Planet9120/Assets/GameManager.cs
@@ -7,14 +7,17 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     PlayerController PlayerScript;
-    GameObject Player;
-    public Text ResourceBox;
-    public Text ShipResourceBox;
+    GameObject Player;    
     float PlayerHealth;
     float ShipFixed;
     public float WinCondition = 100;
+    [Header("UI")]
+    public Text ResourceBox;
+    public Text ShipResourceBox;
     public GameObject deathPanel;
     public GameObject WinPanel;
+    public GameObject PausePanel;
+    bool isPaused;
 
     // Start is called before the first frame update
     void Start()
@@ -26,24 +29,51 @@ public class GameManager : MonoBehaviour
     }
     public void StartOver()
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex, LoadSceneMode.Single);
     }
+
+    public void PauseGame()
+    {
+        PausePanel.SetActive(true);
+        isPaused = true;
+        Time.timeScale = 0f;
+    }
+
+    public void ResumeGame()
+    {
+        PausePanel.SetActive(false);
+        isPaused = false;
+        Time.timeScale = 1f;
+    }
+    
     // Update is called once per frame
     void Update()
     {
         ResourceBox.text = PlayerScript.Resources.ToString();
-        ShipResourceBox.text = PlayerScript.ShipResources.ToString();
+        ShipResourceBox.text = PlayerScript.ShipResources.ToString() + " / " + WinCondition.ToString();
 
         PlayerHealth = PlayerScript.Health;
         if(PlayerHealth <= 0)
         {
+            Time.timeScale = 0f;
             deathPanel.SetActive(true);
             //death
         }
         ShipFixed = PlayerScript.ShipResources;
         if(ShipFixed >= WinCondition)
         {
+            Time.timeScale = 0f;
             WinPanel.SetActive(true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && isPaused == false)
+        {
+            PauseGame();
+
+        }else if (Input.GetKeyDown(KeyCode.Space) && isPaused == true)
+        {
+            ResumeGame();
         }
     }
 }
