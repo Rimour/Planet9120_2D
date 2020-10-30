6577647c155092bb888ada89526579e1b82e8bdc
DHendershot28 <DHendershot@fullsail.edu>
2020-10-30 16:31:41 -0400

    Added two new enemies
    Created a basic function for the two new enemy types

---
+++ Planet9120/Assets/Scripts/EnemyShootScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShootScript : MonoBehaviour
+{
+    public float Range;
+    Vector2 Direction;
+    public GameObject target;
+    public GameObject Gun;
+    public GameObject bullet;
+    private bool CanAttack = true;
+    private float attackTimer;
+    private float cooldown = 1;
+    float nextTimeToFire = 0;
+    public Transform Shootpoint;
+    public float Force;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //shoot();
+        //attackTimer += Time.deltaTime;
+    }
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            shoot();
+        }
+    }
+
+    void shoot()
+    {
+
+           // if (attackTimer >= cooldown)
+          //  {
+                Vector2 targetPos = target.transform.position;
+                Direction = targetPos - (Vector2)transform.position;
+                Gun.transform.up = Direction;
+                GameObject BulletIns = Instantiate(bullet, Shootpoint.position, Quaternion.identity);
+                BulletIns.GetComponent<Rigidbody2D>().AddForce(Direction * Force);
+                SoundManager.PlaySound("TowerShoot");
+                attackTimer = 0;
+         //   }
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position, Range);
+    }
+}
