d0ce6e5dab9bdf0e7c6167e1f1f5a58cd82473b7
DHendershot28 <DHendershot@fullsail.edu>
2020-11-15 18:29:40 -0500

    Ship indicator system finished

---
+++ Planet9120/Assets/Scripts/ShipLocatorScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipLocatorScript : MonoBehaviour
+{
+    public float moveSpeed;
+    public Transform Target;
+    public Rigidbody2D rb;
+    private Vector2 moveDirection;
+    private Vector2 Direction;
+    public GameObject Arrow;
+
+    void Start()
+    {
+    }
+    void Update()
+    {
+        Vector2 targetPos = Target.position;
+        Direction = targetPos - (Vector2)transform.position;
+        Arrow.transform.up = Direction;
+        Move();
+        ProcessInputs();
+    }
+    void ProcessInputs()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        moveDirection = new Vector2(moveX, moveY).normalized;
+    }
+    void Move()
+    {
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+    }
+}
